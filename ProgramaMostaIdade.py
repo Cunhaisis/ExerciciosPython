a68130077d2ed2d8366cfe2f2f3181fc910b383e
IsisCunha <37351239+IsisCunha@users.noreply.github.com>
2019-08-20 12:02:03 -0300

    Initial commit

---
+++ ProgramaMostaIdade.py
@@ -0,0 +1,9 @@
+#Mostra  a Idade e se o usuário é maior ou menor de idade
+
+idade = input ("Digite a sua idade: ")
+
+if (float (idade) >= 18 ):
+    print ("Você é maior de idade")
+else:
+    print ("Você é menor de idade")
+
